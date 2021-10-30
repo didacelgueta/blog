df8a76899260069f86deade900f4a9caa16c4c8c
Didac Elgueta <didac.elgueta.gil@hp.com>
2021-10-30 20:43:10 +0200

    first commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<head>
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+
+</head>
+
+<body>
+    <article>
+        <?= $post; ?>
+    </article>
+    <a href="/">Go Back</a>
+</body>
+
+</html>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<head>
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+
+</head>
+
+<body>
+    <article>
+        <h1><a href="/posts/my-first-post">My First Post</a></h1>
+        <p>
+            We and our partners store or access information on devices, such as cookies and process personal data, such as unique identifiers and standard information sent by a device for the purposes described below. You may click to consent to our and our partners’ processing for such purposes. Alternatively, you may click to refuse to consent, or access more detailed information and change your preferences before consenting. Your preferences will apply to this website only. Please note that some processing of your personal data may not require your consent, but you have a right to object to such processing. You can change your preferences at any time by returning to this site or visit our privacy policy
+        </p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-second-post">My Second </a></h1>
+        <p>
+            We and our partners store or access information on devices, such as cookies and process personal data, such as unique identifiers and standard information sent by a device for the purposes described below. You may click to consent to our and our partners’ processing for such purposes. Alternatively, you may click to refuse to consent, or access more detailed information and change your preferences before consenting. Your preferences will apply to this website only. Please note that some processing of your personal data may not require your consent, but you have a right to object to such processing. You can change your preferences at any time by returning to this site or visit our privacy policy
+        </p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
+        <p>
+            We and our partners store or access information on devices, such as cookies and process personal data, such as unique identifiers and standard information sent by a device for the purposes described below. You may click to consent to our and our partners’ processing for such purposes. Alternatively, you may click to refuse to consent, or access more detailed information and change your preferences before consenting. Your preferences will apply to this website only. Please note that some processing of your personal data may not require your consent, but you have a right to object to such processing. You can change your preferences at any time by returning to this site or visit our privacy policy
+        </p>
+    </article>
+</body>
+
+</html>
